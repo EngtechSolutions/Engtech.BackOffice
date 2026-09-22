ea418c0e2ba64a80fa1d1be52cbba7a842243de3
Arsenio Inojosa <a.inojosa@engtech.com.ve>
2026-09-22 10:33:15 -0400

    feat: Remove BookStore Application layer services and mappers

--- src/Engtech.BackOffice.Application/Authors/AuthorExcelDownloadTokenCacheItem.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Engtech.BackOffice.Authors;
-
-[Serializable]
-public class AuthorExcelDownloadTokenCacheItem
-{
-    public string Token { get; set; } = string.Empty;
-}
--- src/Engtech.BackOffice.Application/Books/BookExcelDownloadTokenCacheItem.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Engtech.BackOffice.Books;
-
-[Serializable]
-public class BookExcelDownloadTokenCacheItem
-{
-    public string Token { get; set; } = string.Empty;
-}
